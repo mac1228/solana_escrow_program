e3fdbe2c6e22e6acd6194bad8e9f54e7138a43aa
micurd <micurd@microsoft.com>
2022-02-01 01:38:26 -0800

    show offers received

--- app/src/Routes/Offers.tsx
+++ app/src/Routes/Offers.tsx
@@ -3,31 +3,25 @@ import { HeaderBar, OfferItem } from "Components";
 import { EscrowContext } from "Helper";
 import * as anchor from "@project-serum/anchor";
 import { Grid } from "@mui/material";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 export function Offers() {
-  const { program, provider } = useContext(EscrowContext);
-  const [myOffers, setMyOffers] = useState<anchor.ProgramAccount[]>();
+  const { provider } = useContext(EscrowContext);
+  const [ownedTokenAccounts, setOwnedTokenAccounts] = useState<any>();
 
   useEffect(() => {
-    const getMyOffers = async (
-      program: anchor.Program,
-      provider: anchor.Provider
-    ) => {
-      const offers = await program.account.offer.all([
-        {
-          memcmp: {
-            offset: 8,
-            bytes: provider.wallet.publicKey.toBase58(),
-          },
-        },
-      ]);
-      console.log(offers);
-      setMyOffers(offers);
+    const getTokenAccounts = async (provider: anchor.Provider) => {
+      const tokenAccounts =
+        await provider.connection.getParsedTokenAccountsByOwner(
+          provider.wallet.publicKey,
+          { programId: TOKEN_PROGRAM_ID }
+        );
+      setOwnedTokenAccounts(tokenAccounts);
     };
-    if (program && provider) {
-      getMyOffers(program, provider);
+    if (provider) {
+      getTokenAccounts(provider);
     }
-  }, [program, provider]);
+  }, [provider]);
 
   return (
     <>
@@ -40,23 +34,106 @@ export function Offers() {
         }}
       >
         <h1>Offers Made</h1>
-        {myOffers && (
-          <Grid
-            style={{ width: "95%" }}
-            container
-            spacing={2}
-            justifyContent={"flex-start"}
-            alignItems={"center"}
-          >
-            {myOffers.map((offer) => (
-              <Grid item key={offer.publicKey.toBase58()}>
-                <OfferItem offer={offer} />
-              </Grid>
-            ))}
-          </Grid>
-        )}
+        <OffersMadeGrid />
         <h1>Offers Received</h1>
+        {ownedTokenAccounts && (
+          <OffersReceivedGrid tokenAccounts={ownedTokenAccounts} />
+        )}
       </div>
     </>
   );
+}
+
+function OffersMadeGrid() {
+  const { program, provider } = useContext(EscrowContext);
+
+  const [offersMade, setOffersMade] = useState<anchor.ProgramAccount[]>();
+
+  useEffect(() => {
+    const getOffers = async (
+      program: anchor.Program,
+      provider: anchor.Provider
+    ) => {
+      const fetchedOffersMade = await program.account.offer.all([
+        {
+          memcmp: {
+            offset: 8,
+            bytes: provider.wallet.publicKey.toBase58(),
+          },
+        },
+      ]);
+      setOffersMade(fetchedOffersMade);
+    };
+    if (program && provider) {
+      getOffers(program, provider);
+    }
+  }, [program, provider]);
+
+  return offersMade ? (
+    <Grid
+      style={{ width: "95%" }}
+      container
+      spacing={2}
+      justifyContent={"flex-start"}
+      alignItems={"center"}
+    >
+      {offersMade.map((offer) => (
+        <Grid item key={offer.publicKey.toBase58()}>
+          <OfferItem offer={offer} />
+        </Grid>
+      ))}
+    </Grid>
+  ) : null;
+}
+
+interface IOffersReceivedGrid {
+  tokenAccounts: any;
+}
+
+function OffersReceivedGrid(props: IOffersReceivedGrid) {
+  const { program } = useContext(EscrowContext);
+  const { tokenAccounts } = props;
+  const [offersReceived, setOffersReceived] = useState<any[]>();
+
+  useEffect(() => {
+    const getOffers = async (program: anchor.Program) => {
+      const promiseOffers: Promise<any>[] = [];
+      tokenAccounts.value.forEach((token: any) => {
+        const offers = program.account.offer.all([
+          {
+            memcmp: {
+              offset: 72,
+              bytes: token.pubkey.toBase58(),
+            },
+          },
+        ]);
+        promiseOffers.push(offers);
+      });
+      const fetchedOffers = await Promise.all(promiseOffers);
+      const singleOffers = fetchedOffers
+        .filter((offer) => offer.length)
+        .map((offer) => offer[0]);
+      setOffersReceived(singleOffers);
+    };
+
+    if (program) {
+      getOffers(program);
+    }
+  }, [program, tokenAccounts]);
+
+  return offersReceived ? (
+    <Grid
+      style={{ width: "95%" }}
+      container
+      spacing={2}
+      justifyContent={"flex-start"}
+      alignItems={"center"}
+    >
+      {offersReceived.map((offer) => (
+        <Grid item key={offer.publicKey.toBase58()}>
+          <OfferItem offer={offer} />
+        </Grid>
+      ))}
+    </Grid>
+  ) : null;
 }
